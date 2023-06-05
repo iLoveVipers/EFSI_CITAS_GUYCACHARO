f9dbbc9df0d1c5e374ec03e3a93e796e798c4dc9
TeiToTeo <teo.e.navarro@gmail.com>
2023-06-05 10:28:13 -0300

    bla bla

--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
 function Cita(props) {
-  return <div class="cita">
+  const [eliminado, setEliminado] = useState(false)
+  return !eliminado && <div class="cita">
       <p>Mascota: <span>{props.Mascota}</span></p>
       <p>Dueño: <span>{props.Dueño}</span></p>
       <p>Fecha: <span>{props.Fecha}</span></p>
       <p>Hora: <span>{props.Hora}</span></p>
-      <p>Sintomas: <span>{props.Sintomas}</span></p><button class="button elimnar u-full-width" onClick={()=>}>Eliminar ×</button>
+      <p>Sintomas: <span>{props.Sintomas}</span></p><button class="button elimnar u-full-width" onClick={()=> setEliminado(true)}>Eliminar ×</button>
     </div>
 }
 
